7b6c49f02e8bef003b031a740d1f6e882580d7e3
rohi-code <rohithkonda99@gmail.com>
2025-05-12 14:23:52 +0530

    get

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -3,15 +3,7 @@ const router = express.Router();
 const Booking = require('../models/bookingSchema');
 
 
-router.post('/create', async (req, res) => {
-  try {
-    const newBooking = new Booking(req.body);
-    const savedBooking = await newBooking.save();
-    res.status(201).json(savedBooking);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+
 
 
 router.get('/', async (req, res) => {
@@ -22,5 +14,7 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+
 
 module.exports = router;
