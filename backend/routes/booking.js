27a4bd57880785490b2674df9b9327663f5fabaf
rohi-code <rohithkonda99@gmail.com>
2025-05-12 14:40:47 +0530

    posts

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -6,14 +6,7 @@ const Booking = require('../models/bookingSchema');
 
 
 
-router.get('/', async (req, res) => {
-  try {
-    const bookings = await Booking.find();
-    res.json(bookings);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+
 router.post('/create', async (req, res) => {
   try {
     const newBooking = new Booking(req.body);
