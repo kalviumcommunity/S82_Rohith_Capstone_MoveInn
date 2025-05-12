dcebb8a80af932bd4a46681186cd4e1db3e4b30f
rohi-code <rohithkonda99@gmail.com>
2025-05-12 17:28:36 +0530

    changes made in .env

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,26 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-
 const authRoutes = require('./routes/auth');
 const bookingRoutes = require('./routes/booking');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
-app.use(cors());
-app.use(express.json()); 
-
-
-app.use('/api/auth', authRoutes);     
+app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingRoutes);
-
 
 app.get('/', (req, res) => {
   res.send('Welcome to MoveInn backend API 🚍');
 });
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/moveinnDB')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
     app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
