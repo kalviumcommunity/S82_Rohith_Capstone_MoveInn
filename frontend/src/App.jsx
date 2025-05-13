604712c30d382926f0b386af3589242d6a011c53
unknown <santimallalakshmidhar@gmail.com>
2025-05-13 15:06:54 +0530

    addedbookingform

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignUp'
 import SplashScreen from './pages/splashScreen';
+import HomePage from './pages/HomePage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path ="/" element={<SplashScreen/>}/>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage/>}/>
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
